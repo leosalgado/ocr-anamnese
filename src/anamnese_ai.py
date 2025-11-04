d7241b385b34ab3b7981888a7e0c09ef86f47187
leosalgado <leonardosalgado@protonmail.com>
2025-11-04 20:53:38 -0300

    Adjust prompt

--- src/anamnese_ai.py
+++ src/anamnese_ai.py
@@ -8,7 +8,7 @@ def anamnese_ai_analysis(ocr_text):
         messages=[
             {
                 "role": "user",
-                "content": "Interprete os seguintes dados do paciente e gere um relatório simples com achados, possíveis diagnósticos e sugestões finais em Português do Brasil (PTBR), dividido em Títulos, subtítulos e seções em formato MARKDOWN. "
+                "content": "Interprete os seguintes dados do paciente e gere um relatório simples com achados, possíveis diagnósticos e sugestões finais em Português do Brasil (PTBR), dividido em Títulos, subtítulos e seções em formato MARKDOWN. (SEM # nos titulos): "
                 + str(ocr_text),
             },
         ],
